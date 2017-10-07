a741ed7ea08c8712c73158afdfa7c9064954e5da
Volodymyr Tsurko <sonicpro@ukrpost.ua>
2017-10-07 21:34:06 +0300

    "The Async Ripple Effect" demo added.

---
+++ Example 3-1/Example 3-1.js
@@ -0,0 +1,54 @@
+// This function is from Adrian Hall's blog: https://shellmonger.com/2015/03/24/promises-and-ajax-in-ecmascript-6/
+function ajaxGet(url) {
+    return new Promise(function (resolve, reject) {
+        "use strict";
+        let req = new XMLHttpRequest();
+        req.open("get", url, true);
+        req.onload = function () {
+            if (req.status === 200) {
+                // Fulfill this promise with the response text.
+                resolve(req.response);
+            } else {
+                // Reject this promise.
+                reject(new Error(req.statusText));
+            }
+        };
+
+        req.onerror = function () {
+            // Also reject this promise.
+            reject(new Error("Network error"));
+        };
+
+        req.send();
+    });
+}
+
+// Show pun get by ajax call, then show the "career decision message"
+showPun().then(function () {
+    console.log('Maybe I should stick to programming');
+});
+
+// Show pun (returns promise fulfilled with undefined).
+function showPun() {
+    return getPun().then(function (punText) {
+        console.log(punText);
+    });
+}
+
+// Get pun text asynchronously, returns promise fulfilled with the pun text.
+function getPun () {
+    // ajaxGet() returns a promise that is eventually fulfilled
+    // by json for {content: "The pet store job was ruff!"}
+    return ajaxGet("http://127.0.0.1/JSWP/pun.json").then(function (json) {
+        var pun = JSON.parse(json);
+        return pun.content;
+    })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
+
+// Console output:
+// The pet store job was ruff!
+// Maybe I should stick to programming
+
