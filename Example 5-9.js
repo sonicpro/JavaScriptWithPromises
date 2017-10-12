57650830a640c17fdb20a3f3231396e85aebbe16
sonicpro <sonic@ukrpost.net>
2017-10-12 11:08:53 +0300

    Mimicing catch/finally with catch/then.

---
+++ Example 5-9.js
@@ -0,0 +1,27 @@
+// Example 5-9.js. Mimicing catch/finally with catch/then.
+
+function getData() {
+    var dataPromise,
+        timestamp = Date.now();
+
+    dataPromise = new Promise(function (resolve, reject) {
+        // ...
+        throw new Error('Unexpected problem');
+    });
+
+    dataPromise.catch(function (err) {
+        // Do not rethrow an error so that the subsequent then() call fires.
+    }).then(function () {
+        // Simulates finally block
+        console.log('Data fetch took ' + (Date.now() - timestamp) + ' ms');
+    });
+
+    // Return data promise instead of catch/then tail to propagate rejection.
+    return dataPromise;
+}
+
+getData().catch(function (e) {
+    console.log('Some error occurred');
+    console.log(e);
+});
+    
