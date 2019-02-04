6ec7839866bc3925f0f30f7a27226ea942906d26
sonicpro <sonic@ukrpost.net>
2019-02-04 11:15:29 +0200

    "Race condition" in async JavaScript demo commented

--- Example 1-12.js
+++ Example 1-12.js
@@ -2,13 +2,14 @@ var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var async = true;
 var xhr = new XMLHttpRequest();
 xhr.open('get', 'data.json', async);
-xhr.send();
-
-setTimeout(function delayed() { // Creates race condition!
+xhr.send();						// The result of AJAX call can be removed from the queue as soon as the execution
+								// of the current script completes.
     function listener() {
         console.log('greetings from listener');
     }
+
+setTimeout(function delayed() { // Creates race condition!
     xhr.addEventListener('load', listener);
     xhr.addEventListener('error', listener);
-}, 1030);
+}, 5);
 
